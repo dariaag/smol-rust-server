617d82b1326497d1fbe4ecd12624ab13c022d083
Daria <dariaagad@gmail.com>
2023-06-13 23:32:10 +0200

    adds path

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -11,6 +11,17 @@ impl WebsiteHandler {
     }
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(&path).ok()
+                } else {
+                    println!("Directory traversal attacj attempted: {}", path);
+                    None
+                }
+            }
+            Err(_) => None,
+        };
         fs::read_to_string(path).ok()
     }
 }
@@ -22,7 +33,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match req.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
